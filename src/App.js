6c2879c29d40b763eecc269176191cf7acda676a
lkbhargav <lkbhargav9@gmail.com>
2017-10-13 00:48:43 -0400

    Basic LESS file structuring is complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Styles.css';
 
 class App extends Component {
   render() {
@@ -8,7 +9,13 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>This is h1</h1>
+          <h2>This is h2</h2>
+          <h3>This is h3 </h3>
+          <p>This is simple paragraph. </p>
+          <ul>
+              <span className="nav-item">/</span><li className="nav-item"> First Item </li>
+          </ul>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
